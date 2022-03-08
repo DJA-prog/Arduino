5bb6a67c4d2e8e652cd9979ece0bc845c3881959
Dino Jose Almirall <almirall.dino@gmail.com>
2022-03-08 17:42:09 +0200

    Update esp12f_code.ino

--- ESP-12F-greenhouse-V_1.0/esp12f_code/esp12f_code.ino
+++ ESP-12F-greenhouse-V_1.0/esp12f_code/esp12f_code.ino
@@ -1,3 +1,14 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+const char* ssid = "HomeADSL";
+const char* password = "almirall";
+
+const char* serverName = "http://192.168.178.59:80/post/index.php";
+unsigned long lastTime = 0;
+unsigned long timerDelay = 2000; //milli second
+
 int mux_s1 = 14; // A
 int mux_s2 = 12; // B
 int mux_s3 = 13; // C
@@ -23,23 +34,52 @@ void setup() {
   pinMode(dataPin, OUTPUT);
   pinMode(2, OUTPUT);
   analogWrite(2, 250); // built in LED to indicate power, Note some boards are GPIO1 other GPIO2, and may be inverted or not
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting...");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(200);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  
+  if ((millis() - lastTime) > timerDelay) {
+    if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
+      http.begin(client, serverName);
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      String httpRequestData = "h="+String(h)+"&t="+String(t);        
+      Serial.println(httpRequestData);
+      int httpResponseCode = http.POST(httpRequestData);
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+  
   muxGet();
   for(int i = 0; i < 8; i++){ //update shitout byte from array
     if(shiftd[i] == 1){bitSet(shiftD, i);}
     if(shiftd[i] == 0){bitClear(shiftD, i);}
   }
-  updateShiftRegister();
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  String httpRequestData = String(h)+"   "+String(t); 
+  updateShiftRegister(); 
   
   for(int x=0;x<8;x++){
     Serial.print("Mux("+String(x)+")="+String(mux_z[x])+ "  ");
   }
-  Serial.print(String(shiftD)+"  "+String(httpRequestData));
+  Serial.print(String(shiftD)+"  "+String(h)+"   "+String(t));
   Serial.println();
   
   delay(2000);
@@ -48,12 +88,12 @@ void loop() {
 
 void muxGet(){
   digitalWrite(mux_s1, LOW); digitalWrite(mux_s2, LOW); digitalWrite(mux_s3, LOW); delay(1); mux_z[0] = analogRead(A0);
-  digitalWrite(mux_s1, HIGH); digitalWrite(mux_s2, LOW); digitalWrite(mux_s3, LOW); delay(1); mux_z[1] = analogRead(A0);
+  digitalWrite(mux_s1, HIGH); digitalWrite(mux_s2, LOW); digitalWrite(mux_s3, LOW); delay(1); mux_z[4] = analogRead(A0);
   digitalWrite(mux_s1, LOW); digitalWrite(mux_s2, HIGH); digitalWrite(mux_s3, LOW); delay(1); mux_z[2] = analogRead(A0);
-  digitalWrite(mux_s1, HIGH); digitalWrite(mux_s2, HIGH); digitalWrite(mux_s3, LOW); delay(1); mux_z[3] = analogRead(A0);
-  digitalWrite(mux_s1, LOW); digitalWrite(mux_s2, LOW); digitalWrite(mux_s3, HIGH); delay(1); mux_z[4] = analogRead(A0);
+  digitalWrite(mux_s1, HIGH); digitalWrite(mux_s2, HIGH); digitalWrite(mux_s3, LOW); delay(1); mux_z[6] = analogRead(A0);
+  digitalWrite(mux_s1, LOW); digitalWrite(mux_s2, LOW); digitalWrite(mux_s3, HIGH); delay(1); mux_z[1] = analogRead(A0);
   digitalWrite(mux_s1, HIGH); digitalWrite(mux_s2, LOW); digitalWrite(mux_s3, HIGH); delay(1); mux_z[5] = analogRead(A0);
-  digitalWrite(mux_s1, LOW); digitalWrite(mux_s2, HIGH); digitalWrite(mux_s3, HIGH); delay(1); mux_z[6] = analogRead(A0);
+  digitalWrite(mux_s1, LOW); digitalWrite(mux_s2, HIGH); digitalWrite(mux_s3, HIGH); delay(1); mux_z[3] = analogRead(A0);
   digitalWrite(mux_s1, HIGH); digitalWrite(mux_s2, HIGH); digitalWrite(mux_s3, HIGH); delay(1); mux_z[7] = analogRead(A0);
 }
 
